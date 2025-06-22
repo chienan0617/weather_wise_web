ad6bd34cc5e5b60169273a1d9ff2d5b74162d9d4
chienan0617 <chienan0617@gmail.com>
2025-06-23 07:32:52 +0800

    Weather Wise: Web Alpha: 1.5.4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3677038630"
+    serviceWorkerVersion: "348142685"
   }
 });
